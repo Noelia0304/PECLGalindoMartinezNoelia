0a89adced902d4084b6176b4cd46aee6c967adf6
Noelia0304 <n.galindo@edu.uah.es>
2023-10-20 13:28:10 +0200

    Update Pedido.cpp

--- Pedido.cpp
+++ Pedido.cpp
@@ -2,6 +2,18 @@
 
 Pedido::Pedido()
 {
+void ::generarDNI()
+{
+	char letras[]= "TRWAGMYFPDXBNJZSQVHLCKE";
+	int numDNI = 0, aux = 1E7;
+	for(int i = 0; i < 8; i++) {
+		int num = rand() % 10;
+		this->DNI[i] = '0' + num;
+		numDNI = numDNI + num * aux;
+		aux /= 10;
+	}
+	this->DNI[8] = letras[numDNI % 23];
+	this->DNI[9] = '\0';
 }
 
 Pedido::~Pedido()
