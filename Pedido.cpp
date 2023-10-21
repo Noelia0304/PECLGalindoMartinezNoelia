43d708168b81cac5b3cc871a5a76ebe4d3de2b9b
Noelia0304 <n.galindo@edu.uah.es>
2023-10-21 14:23:30 +0200

    Update Pedido.cpp

--- Pedido.cpp
+++ Pedido.cpp
@@ -6,22 +6,29 @@ Pedido::Pedido()
     this ->generarDNI();
 }
 
-void::generarID(bool esUrgente){
+void Pedido::generarID(bool esUrgente){
     if(esUrgente){
         id = (rand() % 51) + 49;
     } else{
         id = (rand() % 50) + 1;
     }   
 }
-void::generarNSeguimiento(bool esUrgente){
+void Pedido::generarNSeguimiento(bool esUrgente){
     if(esUrgente){
         nSeguimiento = (rand() % 501) + 499;
     } else{
         nSeguimiento = (rand() % 499) + 1;
     }
-        
 }
-void ::generarDNI()
+void Pedido::mostrar()
+{
+    string urgencia = "Estandar";
+    if(esUrgente){
+        urgencia = "Urgente";
+    }
+    cout << "\t" << setw(10) << DNI << "con ID" << id << "del tipo" << urgencia << "con numero de seguimiento" << nSeguimiento << endl;
+}
+void Pedido::generarDNI()
 {
 	char letras[]= "TRWAGMYFPDXBNJZSQVHLCKE";
 	int numDNI = 0, aux = 1E7;
