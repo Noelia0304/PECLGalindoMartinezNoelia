605acd49825984fbd6b8e044677adcd6b4f53be8
Noelia0304 <n.galindo@edu.uah.es>
2023-10-23 19:19:24 +0200

    Update Lista.cpp

--- Lista.cpp
+++ Lista.cpp
@@ -2,6 +2,28 @@
 
 Lista::Lista()
 {
+    primero = NULL;
+    longitud = 0;
+}
+void Lista::insertar(Pedido pedido)
+{
+    pnodoPila nuevo;
+    nuevo = new NodoPila(pedido, ultimo);
+    ultimo = nuevo;
+    longitud++;
+}
+int Lista::extraer()
+{
+    pnodoPila nodo;
+    Pedido pedido;
+    if(!ultimo)
+        return 0;
+    nodo = ultimo;
+    ultimo = nodo->siguiente;
+    pedido = nodo->valor;
+    longitud--;
+    delete nodo;
+    return pedido;
 }
 
 Lista::~Lista()
