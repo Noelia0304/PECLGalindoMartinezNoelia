8ecf56be95b3c3c758d5305485ba32a52f25ec1a
eliaencinas <145619894+eliaencinas@users.noreply.github.com>
2023-10-24 20:16:41 +0200

    Update NodoPila.hpp

--- NodoPila.hpp
+++ NodoPila.hpp
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 #ifndef NODOPILA_HPP
 #define NODOPILA_HPP
 
+=======
+#include <iostream> 
+#include "Pedido.hpp"
+using namespace std;
+>>>>>>> 2f454e902f5b14e6a8bed2d5056e472b577e14fb
 
 class NodoPila
 {
