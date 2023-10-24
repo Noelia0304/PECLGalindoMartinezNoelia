337a93854e5d70fdc62cf8de3365ca091420f7e0
eliaencinas <145619894+eliaencinas@users.noreply.github.com>
2023-10-24 12:29:01 +0200

    Update Gestor.cpp

--- Gestor.cpp
+++ Gestor.cpp
@@ -1,21 +1,21 @@
 #include "Gestor.hpp"
 
-std::vector<Pedido> crearLista(){
-    std::vector<Pedido> lista;
-    
-    for(int i = 0; i<12; ++i){
-        Pedido unPedido;
-        lista.push_back(Pedido(unPedido.id, unPedido.dniCliente + std::to_string(unPedido.id), unPedido.nSeguimiento, unPedido.esUrgente)); 
-    }
-    
-    for (const auto& unPedido : lista){
-        unPedido.mostrar();
-    }
-    
-}
 
 Gestor::Gestor()
 {
+}
+
+Gestor::crearLista(){
+    
+    for(int i = 0; i<12; ++i){ \\ no es lista, es pedido
+        Pedido unPedido;
+        Lista listaPedidos;
+        listaPedidos.insertar(unPedido);
+    }
+    
+    for (int i = 0; i< 12; ++i){
+         pilaPedidos.mostrar();
+    }
 }
 
 Gestor::~Gestor()
