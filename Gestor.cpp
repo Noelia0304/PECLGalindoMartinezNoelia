57141ecfced5543c460f5c30765a995c49694055
eliaencinas <145619894+eliaencinas@users.noreply.github.com>
2023-10-24 17:03:37 +0200

    Update Gestor.cpp

--- Gestor.cpp
+++ Gestor.cpp
@@ -5,15 +5,20 @@ Gestor::Gestor()
 {
 }
 
-Gestor::generar12Pedido(){
+void Gestor::generar12Pedidos(){
     if (pilaPedidos.size()<= 36){
         for(int i = 0; i<12; ++i){ \\ no es lista, es pedido
             Pedido unPedido;
             pilaPedidos.insertar(unPedido);
         }
-    fo    }
+    }
     
-    for (int i = 0; i< 12; ++i){
+    
+}
+
+void Gestor::muestraPedidos(){
+    
+    for (int i = 0; i< pilaPedidos.size(); ++i){
          pilaPedidos.mostrar();
     }
 }
