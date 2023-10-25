5feb88dd893f403b8352c3686333cfd00c04b1dc
Noelia0304 <n.galindo@edu.uah.es>
2023-10-25 11:17:01 +0200

    Update Gestor.cpp

--- Gestor.cpp
+++ Gestor.cpp
@@ -5,7 +5,9 @@ Gestor::Gestor()
 {
     
 }
-
+int Gestor::PedidosEnPila(){
+   return pilaPedidos.getLongitud();
+}
 void Gestor::generar12Pedidos(){
     if (pilaPedidos.getLongitud()<= 36){
         for(int i = 0; i<12; ++i){
