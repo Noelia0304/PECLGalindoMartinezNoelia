60c473b7920545aecfc63a30ff9732a913fa0209
Noelia0304 <n.galindo@edu.uah.es>
2023-11-28 12:41:34 +0100

    Update NodoArbol.cpp

--- NodoArbol.cpp
+++ NodoArbol.cpp
@@ -1,8 +1,8 @@
 #include "NodoArbol.hpp"
 
-NodoArbol::NodoArbol(Pedido dato)
+NodoArbol::NodoArbol(Pedido pedido)
 {
-    this->dato = dato;
+    this->pedido = pedido;
     this->izq = nullptr;
     this->der = nullptr;
 }
